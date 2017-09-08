19874308e139296d3c56d5b6657e3cbefb01b6db
Bob van Luijt <bob@kubrickolo.gy>
2017-09-08 19:25:25 +0300

    Release a simple first version! 🎇🎇🎇

--- whereWasBob.py
+++ whereWasBob.py
@@ -5,21 +5,27 @@
 import sys, urllib.request, json
 
 longLatArray = []
+nowHere = True
 
 def getList(offSet):
     print("Load with offset: " + str(offSet))
     token = sys.argv[1]
-    with urllib.request.urlopen("https://api.foursquare.com/v2/users/23018068/checkins?offset=" + str(offSet) + "&limit=250&oauth_token=" + token + "&v=20170908") as url:
+    with urllib.request.urlopen("https://api.foursquare.com/v2/users/23018068/checkins?sort=newestfirst&offset=" + str(offSet) + "&limit=250&oauth_token=" + token + "&v=20170908") as url:
         mainData = json.loads(url.read().decode())
         for data in mainData['response']['checkins']['items']:
             if "venue" in data:
                 if len(data["venue"]["categories"]) != 0:
-                    longLatArray.append({
-                        "name": data["venue"]["name"],
-                        "lng": data["venue"]["location"]["lng"],
-                        "lat": data["venue"]["location"]["lat"],
-                        "type": data["venue"]["categories"][0]["name"]
-                    })
+                    if data["venue"]["categories"][0]["name"] != "Home (private)":
+                        global nowHere
+                        longLatArray.append({
+                            "name": data["venue"]["name"],
+                            "lng": data["venue"]["location"]["lng"],
+                            "lat": data["venue"]["location"]["lat"],
+                            "type": data["venue"]["categories"][0]["name"],
+                            "now": nowHere
+                        })
+                        nowHere = False
+
     if mainData["response"]["checkins"]["count"] - offSet > 0:
         getList(offSet + 250)
 
