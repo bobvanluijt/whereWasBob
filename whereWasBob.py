03c4d6e26bd1655c089674205b3f5afef9d6144d
Bob van Luijt <bob@kubrickolo.gy>
2017-09-08 18:11:53 +0300

    First commit

---
+++ whereWasBob.py
@@ -0,0 +1,28 @@
+##
+# Gets the latest tokens
+##
+
+import sys, urllib.request, json
+
+longLatArray = []
+
+def getList(offSet):
+    print("Load with offset: " + str(offSet))
+    token = sys.argv[1]
+    with urllib.request.urlopen("https://api.foursquare.com/v2/users/23018068/checkins?offset=" + str(offSet) + "&limit=250&oauth_token=" + token + "&v=20170908") as url:
+        mainData = json.loads(url.read().decode())
+        for data in mainData['response']['checkins']['items']:
+            if "venue" in data:
+                if len(data["venue"]["categories"]) != 0 and data["venue"]["categories"][0]["name"] == "Airport":
+                    longLatArray.append({
+                        "name": data["venue"]["name"],
+                        "lng": data["venue"]["location"]["lng"],
+                        "lat": data["venue"]["location"]["lat"]
+                    })
+    if mainData["response"]["checkins"]["count"] - offSet > 0:
+        getList(offSet + 250)
+
+getList(0)
+
+with open('whereWasBob.json', 'w') as outfile:
+    json.dump(longLatArray, outfile)
